27ae26c4a9746ecd43b06130179cc1f0fcda41b3
David Šarman <david.sarman@gmail.com>
2016-08-17 01:22:08 +0200

    Added more tests and fixes.

--- find_combinations.py
+++ find_combinations.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+import argparse
+import copy
 import datetime
 import fileinput
-from typing import Dict, List, Iterable
+from typing import List, Set
 
 
 class Flight:
@@ -27,13 +29,23 @@ class Flight:
                 return True
         return False
 
-    def __str__(self, *args, **kwargs):
+    def __str__(self):
         return "Flight[source: {}, destination: {}, departure: {}, arrival: {}, flight_number: {}]".format(
             self.source, self.destination, self.departure, self.arrival, self.flight_num)
 
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__) and self.flight_num == other.flight_num)
+
+    def __hash__(self):
+        return hash(self.flight_num)
+
 
 class FlightPath:
-    def __init__(self, flight_one, flight_two):
+    def __init__(self, flight_one: Flight, flight_two: Flight):
+        assert flight_one.connects_to(flight_two), "Flights must connect"
         self.path = [flight_one, flight_two]
 
     def try_add(self, flight: Flight) -> bool:
@@ -41,34 +53,70 @@ class FlightPath:
         Tries to add given flight to the flight path.
         Checks for repeating paths A -> B -> A -> B.
         """
-        path_size = len(self.path)
-        if not self.path[path_size].connects_to(flight):
+        last_path_index = len(self.path) - 1
+        if not self.path[last_path_index].connects_to(flight):
             return False
 
-        for i in range(0, path_size - 1):
+        for i in range(0, last_path_index):
             existing_destination = self.path[i].destination
-            existing_source = self.path[i + 1].source
-            if existing_destination == self.path[path_size].destination and existing_source == flight.source:
+            existing_source = self.path[i].source
+            if existing_destination == flight.destination and existing_source == flight.source:
                 return False
         self.path.append(flight)
         return True
 
-    def get_last_path(self):
-        return self.path[len(self.path)]
+    def get_last_flight(self):
+        """
+        Returns last flight in path
+        """
+        return self.path[len(self.path) - 1]
 
-    def __str__(self, *args, **kwargs):
+    def __str__(self):
         out = self.path[0].source
         path_size = len(self.path)
         for i in range(0, path_size):
             out += "-- " + self.path[i].flight_num + " -->" + self.path[i].destination
         return out
 
+    def __len__(self):
+        return len(self.path)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+        size = len(self.path)
+        if size != len(other.path):
+            return False
+        for i in range(0, size - 1):
+            if not self.path[i] == other.path[i]:
+                return False
+        return True
+
+    def __hash__(self):
+        hash_val = hash(self.path[0])
+        for i in range(1, len(self.path) - 1):
+            hash_val += hash(self.path[i])
+        return hash_val
+
 
 class Parser:
-    def __init__(self, time_format="%Y-%m-%dT%H:%M:%S"):
+    def __init__(self, time_format: str = "%Y-%m-%dT%H:%M:%S"):
         self.time_format = time_format
-        self.flights = {}
+        self.flights = []
         self.flights_count = 0
+
+    def parse_file(self, ignore_first=True, file=None):
+        """
+        Parses given file, or stdout if none is supplied
+        :param ignore_first: If True, ignores first line
+        """
+        if ignore_first:
+            input()
+        for line in fileinput.input(file):
+            self.parse_and_add(line)
 
     def parse_line(self, line: str) -> Flight:
         """
@@ -81,54 +129,68 @@ class Parser:
         new_flight = Flight(split_line[0], split_line[1], departure, arrival, split_line[4])
         return new_flight
 
-    def add_flight(self, flight: Flight) -> None:
-        """
-        Adds flight to dictionary of other flights.
-        """
-        existing_flights = self.flights.get(flight.source)
-        if existing_flights is not None:
-            existing_flights.append(flight)
-        else:
-            existing_flights = [flight]
-        self.flights[flight.source] = existing_flights
-        self.flights_count += 1
-
-    def parse_and_add(self, line: str) -> None:
+    def parse_and_add(self, line: str) -> Flight:
         """
         Parses and adds given line.
         """
-        self.add_flight(self.parse_line(line))
+        flight = self.parse_line(line)
+        self.add_flight(flight)
+        return flight
+
+    def add_flight(self, flight: Flight):
+        """
+        Adds flight into the flights list.
+        Used in testing.
+        """
+        self.flights.append(flight)
 
 
 class Matcher:
-    def __init__(self, data: Dict[str, List[Flight]]):
+    def __init__(self, data: List[Flight]):
         self.data = data
-        self.paths = []
-        self.data_size = 0
-        for flights in self.data:
-            self.data_size += len(flights)
+        self._paths = []
 
-    def pairing(self, flights2_iter: Iterable, get_flight):
-        for flights in self.data.values():
-            for flights2 in self.data.values():
-                for f1 in flights:
-                    for f2 in flights2:
-                        f2 = get_flight(f2)
-                        if f1.connects_to(f2):
-                            self.paths.append(FlightPath(f1, f2))
+    def pairing(self):
+        """
+        Does one round of pairing between flights.
+        Creates new flight paths, or appends to existing ones.
+        """
+        if len(self._paths) == 0:
+            second_values = self.data
+            get_flight = lambda x: x
+            first = True
+        else:
+            second_values = self._paths
+            get_flight = lambda x: x.get_last_flight()
+            first = False
 
-    def base_pairing(self):
-        self.pairing(self.data.values(), lambda x: x)
+        for value in second_values:
+            f1 = get_flight(value)
+            for f2 in self.data:
+                if f1.connects_to(f2):
+                    if first:
+                        self._paths.append(FlightPath(f1, f2))
+                    else:
+                        path_copy = copy.copy(value)
+                        added = path_copy.try_add(f2)
+                        if added:
+                            self._paths.append(path_copy)
+
+    def get_paths(self) -> Set[FlightPath]:
+        return set(self._paths)
+
+    def full_pairing(self):
+        for i in range(0, len(self.data)):
+            self.pairing()
 
 
 def main() -> None:
     parser = Parser()
-    input()  # Skip the first line
-    for line in fileinput.input():
-        parser.parse_and_add(line)
+    parser.parse_file(ignore_first=True)
     matcher = Matcher(parser.flights)
-    matcher.base_pairing()
-    for path in matcher.paths:
+    matcher.full_pairing()
+    paths = matcher.get_paths()
+    for path in paths:
         print(path)
 
 
--- tests.py
+++ tests.py
@@ -1,12 +1,14 @@
-import datetime
 import unittest
-from find_combinations import Parser, FlightPath, Flight
 
-class FlightPathTestCase(unittest.TestCase):
+from find_combinations import Parser, Matcher, FlightPath
 
+
+class ParserTestCase(unittest.TestCase):
     def setUp(self):
         self.parser = Parser()
 
+
+class FlightTestCase(ParserTestCase):
     def test_simple_connect(self):
         f1 = self.parser.parse_line("BWN,DPS,2016-10-11T18:45:00,2016-10-11T21:05:00,PV923")
         f2 = self.parser.parse_line("DPS,BNW,2016-10-11T23:08:00,2016-10-12T01:00:00,PV924")
@@ -24,6 +26,45 @@ class FlightPathTestCase(unittest.TestCase):
         f2 = self.parser.parse_line("DPS,BNW,2016-10-12T01:05:01,2016-10-12T01:00:00,PV924")
 
         self.assertFalse(f1.connects_to(f2))
+
+
+class FlightPathTestCase(ParserTestCase):
+    def test_simple_path(self):
+        f1 = self.parser.parse_line("BNW,DPS,2016-10-11T18:45:00,2016-10-11T21:05:00,PV923")
+        f2 = self.parser.parse_line("DPS,BNW,2016-10-11T23:08:00,2016-10-12T01:00:00,PV924")
+        f3 = self.parser.parse_line("BNW,HKN,2016-10-12T03:05:01,2016-10-12T05:00:00,PV925")
+
+        path = FlightPath(f1, f2)
+        self.assertTrue(path.try_add(f3))
+        self.assertEqual(path.get_last_flight(), f3)
+
+    def test_simple_path_repetition(self):
+        f1 = self.parser.parse_line("BNW,DPS,2016-10-11T18:45:00,2016-10-11T21:05:00,PV923")
+        f2 = self.parser.parse_line("DPS,BNW,2016-10-11T23:08:00,2016-10-12T01:00:00,PV924")
+        f3 = self.parser.parse_line("BNW,DPS,2016-10-12T03:05:01,2016-10-12T05:00:00,PV925")
+
+        path = FlightPath(f1, f2)
+        self.assertFalse(path.try_add(f3))
+        self.assertNotEqual(path.get_last_flight(), f3)
+
+
+class MatcherTestCase(ParserTestCase):
+    def test_simple_matcher(self):
+        f1 = self.parser.parse_and_add("BNW,DPS,2016-10-11T18:45:00,2016-10-11T21:05:00,PV923")
+        f2 = self.parser.parse_and_add("DPS,BNW,2016-10-11T23:08:00,2016-10-12T01:00:00,PV924")
+        f3 = self.parser.parse_and_add("BNW,HKN,2016-10-12T03:05:01,2016-10-12T05:00:00,PV925")
+        f4 = self.parser.parse_and_add("DPS,BNW,2016-10-11T03:05:01,2016-10-11T17:45:00,PV926")
+
+        p1 = FlightPath(f1, f2)
+        p2 = FlightPath(f2, f3)
+        p3 = FlightPath(f4, f1)
+
+        matcher = Matcher(self.parser.flights)
+        matcher.pairing()
+        self.assertIn(p1, matcher._paths)
+        self.assertIn(p2, matcher._paths)
+        self.assertIn(p3, matcher._paths)
+
 
 if __name__ == '__main__':
     unittest.main()
